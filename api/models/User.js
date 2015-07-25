0ed95b3dd9f0b5ab089af43dea6bbe73e16fd4e3
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-07-25 23:53:56 +0000

    Generate User API

---
+++ api/models/User.js
@@ -0,0 +1,34 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  connection: 'postgre',
+  tableName: 'our_users',
+  attributes: {
+    id: {
+      type: 'integer',
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true,
+      columnName: 'the_primary_key'
+    },
+    name: {
+      type: 'string',
+      columnName: 'full_name'
+    },
+    password: {
+      type: 'string',
+      columnName: 'seriously_encrypted_password'
+    },
+    email: {
+      type: 'email',
+      unique: true,
+      columnName: 'email_address'
+    }
+  }
+};
+
